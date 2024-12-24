bffee08565602cbef52cc6c042f73d0b2e5833d9
rahul22061999 <130198557+rahul22061999@users.noreply.github.com>
2024-12-23 19:48:19 -0800

    new changes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const { chats } = require('./data/data');
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send("API is running")
+} );
+
+
+app.get('/api/chat', (req, res) => {
+    res.send(chats)
+});
+
+
+app.get('/api/chat/:id', (req, res) => {
+    const singleChat = chats.find((c) => c._id === req.params.id)
+    res.send(singleChat)
+})
 
 app.listen(5000, console.log("Port running on 5000"))
