99c7453077e16b4a794e4dcc14b39ac48f306950
rahul22061999 <130198557+rahul22061999@users.noreply.github.com>
2024-12-27 21:11:30 -0800

    new

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const { registerUser } = require('../controllers/userControllers');
 
 const router = express.Router();
 
-router.route('/login')
+router.route('/').post(registerUser)
 
 
 module.exports = router
